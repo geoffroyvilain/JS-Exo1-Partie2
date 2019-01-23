953751d360bbf6cb9fc570baf6f6eda7ad2bdb97
geoffroyvilain <vilaingeoffroy02@gmail.com>
2019-01-23 13:39:39 +0100

    exo1 fini

---
+++ assets/js/scrypt.js
@@ -0,0 +1,7 @@
+function change(object, ev)
+{
+   if(ev == "over")
+     object.src = object.src.replace(/image1(..+)$/, 'image1_2$1');
+   else
+     object.src = object.src.replace(/image1_2(..+)$/, 'image1$1');
+}
